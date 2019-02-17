6401233aaa5ed36caf50d89ab95a4f8fae17bcdf
kevinkyan <kevyan1998@gmail.com>
2019-02-17 14:16:05 -0500

    built parsing framework

--- FSM.cpp
+++ FSM.cpp
@@ -1,68 +1,109 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
-
+struct state {
+				int num;
+				string type;
+				string secondType;
+};
+struct transition {
+				int startState;
+				string transition;
+				int endState;
+};
 int main(int argc, char *argv[]) {
-        int currentState;
-        string line;
-        string input = argv[2];
-        ifstream myfile(argv[1]);
-        if(myfile.is_open()) {
-          while(getline(myfile, line)) {
-            istringstream iss(line) {
-              string type;
-              string state;
-              string second;
-              string third;
-            }
-          }
-        }
+				int currentState;
+				//stores accept and start states
+				vector<state> *states = new vector<state>;
+				vector<transition> *transitions = new vector<transition>;
+				string line;
+				string input = argv[2];
+				int nextInput[input.length()];
+				for(int i = 0; i < line.length(); i++) {
+								nextInput[i] = line.at(i);
+				}
+				ifstream myfile(argv[1]);
+				if(myfile.is_open()) {
+								while(getline(myfile, line)) {
+												istringstream iss(line);
+												string type;
+												string firstInput;
+												string secondInput;
+												string thirdInput;
+												if(type == "state") {
+																//create state
+																state newState =
+																{
+																				stoi(firstInput),
+																				secondInput,
+																				thirdInput
+																};
+																//add state to list of states
+																states->push_back(newState);
+												}
+												if(type == "transition") {
+																//create transition
+																transition newTrans =
+																{
+																				stoi(firstInput),
+																				secondInput,
+																				stoi(thirdInput)
+																};
+																transitions->push_back(newTrans);
+												}
+
+								}
+								myfile.close();
+				}
+				else {
+								cout << "Unable to open file";
+				}
 }
 int *makeIntArray(int length)
 {
-        int *p = NULL;
-        if( length > 0 )
-        {
-                p = (int *) malloc(length*sizeof(int));
-        }
-        return p;
+				int *p = NULL;
+				if( length > 0 )
+				{
+								p = (int *) malloc(length*sizeof(int));
+				}
+				return p;
 }
 void deleteIntArray(int *p)
 {
-        if( p != NULL )
-        {
-                free(p);
-        }
+				if( p != NULL )
+				{
+								free(p);
+				}
 }
 int **makeIntMatrix(int width, int height)
 {
-        int **p = (int **) NULL;
-        int *pData = (int *) NULL;
-        if( (width > 0) && (height > 0) )
-        {
-                p = (int **) malloc(width*sizeof(int*));
-                pData = (int *) malloc(width*height*sizeof(int));
-                5 of 6
-                Due 2/17
-                int i = 0;
-                while( i < width )
-                {
-                        p[i] = pData+(i*height);
-                        i = i+1;
-                }
-        }
-        return p;
+				int **p = (int **) NULL;
+				int *pData = (int *) NULL;
+				if( (width > 0) && (height > 0) )
+				{
+								p = (int **) malloc(width*sizeof(int*));
+								pData = (int *) malloc(width*height*sizeof(int));
+								int i = 0;
+								while( i < width )
+								{
+												p[i] = pData+(i*height);
+												i = i+1;
+								}
+				}
+				return p;
 }
 void deleteIntMatrix(int **p)
 {
-        if( p != NULL )
-        {
-                if( p[0] != NULL )
-                {
-                        free(p[0]);
-                }
-                free(p);
-        }
+				if( p != NULL )
+				{
+								if( p[0] != NULL )
+								{
+												free(p[0]);
+								}
+								free(p);
+				}
 }
---
+++ Makefile
@@ -0,0 +1,17 @@
+#
+# Specifiy the target
+all:	Files
+
+# Specify the object files that the target depends on
+# Also specify the object files needed to create the executable
+Files: FSM.o
+	g++ -g FSM.o -o Main
+
+# Specify how the object files should be created from source files
+FSM.o: FSM.cpp
+	g++ -g -c FSM.cpp
+
+# Specify the object files and executables that are generated
+# and need to be removed to re-compile the whole thing
+clean:
+	rm -f *.o Main
